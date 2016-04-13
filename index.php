f2fea1cc05b7e44ec2a2f438050db58243c41eed
Scaphandroid <arl-informatique@gmx.ch>
2016-04-13 20:43:42 +0200

    ajout de la page d'accueil et du style

---
+++ index.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="style.css" />
+	<script src="jquery.js"></script>
+	</head>
+	
+	<body>
+		<h1>Moving Letters</h1>
+		
+		<input id="texte" type="text" maxlength=300 placeholder="enter votre texte"></input>
+		<input id="go" type="submit" value="Move!"></input>
+		
+		<div id="plateau">
+			<p id="texteAffiche"></p>
+		</div>
+		<div id="zoneEnvoi">
+		</div>
+	</body>
+	
+	<script src="scripts.js"></script>
+
+</html>
