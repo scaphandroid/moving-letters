aba980a6db84295c8b689f382ee21ad1d5dd6fa0
Scaphandroid <arl-informatique@gmx.ch>
2016-04-13 20:56:50 +0200

    refactoring des scripts pour une meilleure lisibilité

--- scripts.js
+++ scripts.js
@@ -3,28 +3,32 @@ var longueur = 0 ;
 var click = false ; 
 var texte ; 
 
-
 $(document).ready(function(){
+	
 	$("#go").click(function(){
 		
-		//remise à zéro 
 		comptage = 0 ; 
-		$('#texteAffiche').empty() ;
-		$('#zoneEnvoi').empty() ; 
 		click = false ; 
 		texte = null ; 
 		
+		$('#texteAffiche').empty() ;
+		$('#zoneEnvoi').empty() ; 
+		
 		var tableauTopFin = new Array() ;
 		var tableauLeftFin = new Array() ;
+		
 		longueur = recupereTexteEtSepare().length ;  
+		
 		for (var i=0 ; i < longueur ; ++i){
 			$('<span id="'+i+'">'+recupereTexteEtSepare()[i]+'</span>').appendTo('#texteAffiche') ;	
 		}
+		
 		for (var i=0 ; i < longueur ; ++i){
 			var topFin = ($("#"+i)).offset().top, leftFin = ($("#"+i)).offset().left ;
 			tableauTopFin.push(topFin) ;
 			tableauLeftFin.push(leftFin) ;
 		}
+		
 		for (var i=0 ; i < longueur ; ++i){		
 			$("#"+i).css("position", "absolute") ;
 			lettreInitialisation(i, tableauTopFin[i] , tableauLeftFin[i]) ;		
@@ -35,22 +39,23 @@ $(document).ready(function(){
 })
 
 function recupereTexteEtSepare(){
+	
 	texte = $("#texte").val() ;
+	
 	var tableauCaracteres = new Array() ;
+	
 	for (var i = 0 ; i < texte.length ; ++i){
 		tableauCaracteres.push(texte.charAt(i)) ; 
 	}
-	return tableauCaracteres
+	
+	return tableauCaracteres ;
 }
 
 function lettreInitialisation(idLettre, topFin, leftFin){
 	
 	var haut = 1, bas = 2, gauche = 3, droite = 4 ;
-	
 	var topDebut = topFin, leftDebut = leftFin ;
-	
 	var coteDepart = Math.floor(Math.random() * (4 - 1 + 1) + 1);
-	
 	var topPlateau = $("#plateau").offset().top;
 	var heightPlateau = parseInt($("#plateau").css("height")) ;
 	var leftPlateau = $("#plateau").offset().left ;
@@ -79,20 +84,20 @@ function lettreInitialisation(idLettre, topFin, leftFin){
 	$("#" + idLettre).css('left', leftDebut+'px') ;
 	 
 	 mouvementLettre(idLettre, topFin, leftFin) ; 
-	
 }
 
 function mouvementLettre(idLettre, topFin, leftFin){
+	
 	$("#"+idLettre).animate(
-				{
+			{
 				left: leftFin,
 				top: topFin
-				},
-				2000,
-				'swing',
-				function(){
-					
-				}
-			) ;
+			},
+			2000,
+			'swing',
+			function(){
+						//on peut par exemple implanter une fonction de transmission de ce message en callback
+			}
+	);
 }
 
