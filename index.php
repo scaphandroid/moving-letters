64302cdfe16097ae448c7f215bedf5c7de93e260
Scaphandroid <arl-informatique@gmx.ch>
2016-04-13 20:45:54 +0200

    Refactoring de l'index et du style pour une meilleure lisibilité du code

--- index.php
+++ index.php
@@ -1,25 +1,32 @@
 ﻿<!DOCTYPE HTML>
 
 <html>
+
 	<head>
+	
 		<meta charset="utf-8"/>
 		<link rel="stylesheet" href="style.css" />
-	<script src="jquery.js"></script>
+		
+		<script src="jquery.js"></script>
+		
 	</head>
 	
 	<body>
+	
 		<h1>Moving Letters</h1>
 		
 		<input id="texte" type="text" maxlength=300 placeholder="enter votre texte"></input>
+		
 		<input id="go" type="submit" value="Move!"></input>
 		
 		<div id="plateau">
 			<p id="texteAffiche"></p>
 		</div>
-		<div id="zoneEnvoi">
-		</div>
+		
+		<div id="zoneEnvoi"></div>
+		
+		<script src="scripts.js"></script>
+		
 	</body>
 	
-	<script src="scripts.js"></script>
-
 </html>
